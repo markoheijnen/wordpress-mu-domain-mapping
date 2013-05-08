3352f9a1e9dfa37818749280a44095306153d07d
Marko Heijnen <info@markoheijnen.com>
2013-05-08 21:47:45 +0200

    Remove unneeded filters. MP6 was broken and by removing everything still works fine

--- domain_mapping.php
+++ domain_mapping.php
@@ -659,18 +659,7 @@ function redirect_login_to_orig() {
 	}
 }
 
-// fixes the plugins_url 
-function domain_mapping_plugins_uri( $full_url, $path=NULL, $plugin=NULL ) {
-	return get_option( 'siteurl' ) . substr( $full_url, stripos( $full_url, PLUGINDIR ) - 1 );
-}
-
-function domain_mapping_themes_uri( $full_url ) {
-	return str_replace( get_original_url ( 'siteurl' ), get_option( 'siteurl' ), $full_url );
-}
-
 if ( defined( 'DOMAIN_MAPPING' ) ) {
-	add_filter( 'plugins_url', 'domain_mapping_plugins_uri', 1 );
-	add_filter( 'theme_root_uri', 'domain_mapping_themes_uri', 1 );
 	add_filter( 'pre_option_siteurl', 'domain_mapping_siteurl' );
 	add_filter( 'pre_option_home', 'domain_mapping_siteurl' );
 	add_filter( 'the_content', 'domain_mapping_post_content' );
